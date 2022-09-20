86cb56bf2280e8c95169c4f05a259afcd73e510c
Santhosh M <santhoshm@Santhoshs-MacBook-Air.local>
2022-09-20 15:16:42 +0530

    First commit

---
+++ src/Pages/_app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return(<>
+    <Head>
+      <meta
+        name="viewport"
+        content="initial-scale=1,width=device-width,viewport-fit=cover,maximum-scale=1.0,user-scalable=no"
+      />
+      <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>)
+  
+}
+export default MyApp;
---
+++ src/Pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "../App";
+
+const Index=()=>{
+    return(
+        <>
+      <App/>
+       </>
+       )
+ }
+export default Index;
